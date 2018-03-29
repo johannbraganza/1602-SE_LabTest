30ab5fed2c6c6d2da79d27403537d27c7da34edd
Johann <Johann@DESKTOP-4S84J89>
2018-03-29 14:54:19 +0530

    First Build

---
+++ src/Cricket/MainClass.java
@@ -0,0 +1,11 @@
+package Cricket;
+import java.util.*;
+public class MainClass 
+{
+	public static void main(String args[])
+	{
+		System.out.println("Cricket Match ScoreCard App");
+		
+		
+	}
+}
---
+++ src/Cricket/ScoreCal.java
@@ -0,0 +1,5 @@
+package Cricket;
+
+public class ScoreCal {
+
+}
